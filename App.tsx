e0f360b5d08f19725f5d7042b03c46416ad84f1c
jiho lee <optional.int@kakao.com>
2021-03-19 20:25:00 +0900

    delete: headerRight

--- App.tsx
+++ App.tsx
@@ -76,7 +76,6 @@ export default function App() {
                   component={MainScreen}
                   options={({ navigation, route }) => ({
                     headerLeft: (props) => null,
-                    headerRight: (props) => logout(navigation),
                     title: "Main Page",
                   })}
                 />
