32ba51cf6fa7ed845d5e96807f1af524a4e12145
jiho lee <optional.int@kakao.com>
2021-03-19 20:02:58 +0900

    Improved: error handling

--- sagas/loginSaga.ts
+++ sagas/loginSaga.ts
@@ -11,17 +11,16 @@ export function* loginAsync(action) {
 
   try {
     response = yield Axios.post(`${url}/login`, data);
-    console.log("response", response);
   } catch (error) {
     failed = true;
     console.log(error);
     yield put(loginActions.loginFailedAsync(error));
   }
 
-  console.log("response", response);
-  console.log(response.data);
   if (failed) {
     return;
   }
+  console.log("response", response);
+  console.log(response.data);
   yield put(loginActions.loginAsync(response.data));
 }
