d560c55c2234f0fe69251e5cea3dec3653e03d18
jiho lee <optional.int@kakao.com>
2021-03-19 21:44:10 +0900

    Add: dispatch

--- screens/BoardScreen.tsx
+++ screens/BoardScreen.tsx
@@ -4,12 +4,11 @@ import * as RN from "react-native";
 import * as Paper from "react-native-paper";
 import { Text, View } from "../components/Themed";
 import { useDispatch, useSelector } from "react-redux";
-import { loginActions } from "../slices/loginSlice";
-import { IResponse } from "../types";
 import styles from "../styles/common";
 export default function BoardScreen({ navigation }): React.ReactElement {
+  const dispatch = useDispatch();
   const getBoards = () => {
-    // dispatch(boardsActions.getBoards(navigation));
+    dispatch(boardsActions.getBoards(navigation));
   };
   return (
     <View style={styles.container}>
