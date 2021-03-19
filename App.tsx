c6229230e38496ee89705b2b03c79785bf955038
jiho lee <optional.int@kakao.com>
2021-03-19 15:06:55 +0900

    Fix: using react-navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import RegisterScreen from "./screens/RegisterScreen";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const Stack = createStackNavigator();
+
+  useEffect(() => {
+    console.log("isLoadingComplete:", isLoadingComplete);
+  }, [isLoadingComplete]);
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaProvider>
     );
   }
--- hooks/useOnChange.ts
+++ hooks/useOnChange.ts
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import * as RN from 'react-native';
+import * as React from "react";
+import * as RN from "react-native";
 
 export type Dispatcher<S> = React.Dispatch<React.SetStateAction<S>>;
 export type parameterType = RN.NativeSyntheticEvent<RN.TextInputChangeEventData>;
 export default function useOnChange(
-  setter: Dispatcher<string>,
+  setter: Dispatcher<string>
 ): (e: parameterType) => void {
   return (e: parameterType): void => {
-    setter(e);
+    setter(e.currentTarget);
   };
 }
