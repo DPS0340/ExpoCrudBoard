878c250c18b0bf5e067b8abdf62cd90178d8ca03
jiho lee <optional.int@kakao.com>
2021-03-23 00:21:53 +0900

    Fix: apply color on Component

--- src/components/BackNavigatorComponent.tsx
+++ src/components/BackNavigatorComponent.tsx
@@ -27,6 +27,7 @@ export default function BackNavigatorComponent(props: {
             navigation.goBack();
           }
         }}
+        color={color}
       >
         Back
       </Paper.Button>
