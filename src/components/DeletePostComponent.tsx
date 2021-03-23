62288b37e029fafe3a3caa506202299494be0018
jiho lee <optional.int@kakao.com>
2021-03-24 03:54:56 +0900

    Add: post delete logic

--- src/components/DeletePostComponent.tsx
+++ src/components/DeletePostComponent.tsx
@@ -5,15 +5,48 @@ import * as RN from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import CommentsComponent from "../components/CommentsComponent";
 import HTML from "react-native-render-html";
+import { useDispatch, useSelector } from "react-redux";
+import { postsActions, postsReducers } from "../slices/postsSlice";
+import useEffectWithInitialCallback from "../hooks/useEffectWithInitialCallback";
 
 export default function DeletePostComponent(props: {
   navigation: StackNavigationHelpers;
   pk: number;
+  boardPk: number;
 }): React.ReactElement {
   const { navigation, pk } = props;
+  const { responseData, isLoading, isSuccess, error } = useSelector(
+    (state) => ({
+      responseData: state.postsReducers.responseData,
+      isLoading: state.postsReducers.isLoading,
+      isSuccess: state.postsReducers.isSuccess,
+      error: state.postsReducers.error,
+    })
+  );
+  const dispatch = useDispatch();
   const onClick = () => {
-    alert("TODO");
+    dispatch(postsActions.deletePost({ pk }));
   };
+  useEffectWithInitialCallback(
+    () => {
+      dispatch(postsActions.resetStatus());
+    },
+    () => {
+      if (isLoading) {
+        return;
+      }
+      if (isSuccess) {
+        console.log({ responseData });
+        dispatch(postsActions.reset());
+        if (navigation.canGoBack()) {
+          navigation.goBack();
+        }
+        return;
+      }
+      console.log({ error });
+    },
+    [isLoading, isSuccess]
+  );
   return (
     <Paper.Button mode="contained" onPress={onClick}>
       Delete Post
--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -5,12 +5,12 @@ import { registerActions } from "../slices/registerSlice";
 import { registerAsync } from "./registerSaga";
 import { getBoardsAsync } from "./boardsSaga";
 import { checkLoginAsync, loginAsync, logoutAsync } from "./loginSaga";
-import { getPostsAsync, writePostAsync } from "./postsSaga";
+import { deletePostAsync, getPostsAsync, writePostAsync } from "./postsSaga";
 import { postsActions } from "../slices/postsSlice";
 import { commentsActions } from "../slices/commentsSlice";
 import { getCommentsAsync } from "./commentsSaga";
 
-const { getPosts, writePost } = postsActions;
+const { getPosts, writePost, deletePost } = postsActions;
 const { getBoards } = boardsActions;
 const { login, checkLogin, logout } = loginActions;
 const { register } = registerActions;
@@ -22,6 +22,7 @@ export default function* rootWatcher() {
   yield takeLatest(checkLogin.type, checkLoginAsync);
   yield takeLatest(writePost.type, writePostAsync);
   yield takeLatest(logout.type, logoutAsync);
+  yield takeLatest(deletePost.type, deletePostAsync);
   yield takeEvery(getBoards.type, getBoardsAsync);
   yield takeEvery(getPosts.type, getPostsAsync);
   yield takeEvery(getComments.type, getCommentsAsync);
