447a3defe5d5bea51306c237021f05c58d80482e
jiho lee <optional.int@kakao.com>
2021-03-26 01:51:31 +0900

    Fix: View -> ScroolView

--- src/components/ResponsiveComponent.tsx
+++ src/components/ResponsiveComponent.tsx
@@ -6,7 +6,7 @@
 import * as React from "react";
 import styled from "styled-components/native";
 import * as RN from "react-native";
-import px2vw from "../../utils/px2vw";
+import * as Paper from "react-native-paper";
 
 const ResponsiveBlock = styled.View`
   display: flex;
@@ -29,7 +29,7 @@ function Responsive(props: {
   const { children, ...rest } = props;
   // style, className, onClick, onMouseMove 등의 props를 사용할 수 있도록
   // ...rest를 사용하여 ResponsiveBlock에게 전달
-  const Block = RN.Platform.OS === "web" ? ResponsiveBlock : RN.View;
+  const Block = RN.Platform.OS === "web" ? ResponsiveBlock : RN.ScrollView;
   return <Block {...rest}>{children}</Block>;
 }
 
